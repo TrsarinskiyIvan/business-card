345e30eed5095c06e84b342f2d3c1bac7b7649a6
Ivan Tsarinskiy <itsarinskiy@gmail.com>
2022-07-17 10:59:14 +0300

    drop-down menu

---
+++ scripts/drop-menu.js
@@ -0,0 +1,10 @@
+
+function showMenu() {
+    let dropMenu = document.getElementById('dropMenu');
+    dropMenu.classList.add('nav-show');
+}
+
+function hideMenu() {
+    let dropMenu = document.getElementById('dropMenu');
+    dropMenu.classList.remove('nav-show');
+}
